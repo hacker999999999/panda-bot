0a4fe7e5e42467764ce650242458d590ecd77c00
SHVANx69 <78313191+SHVANx69@users.noreply.github.com>
2021-07-06 12:37:50 +0300

    Update daily.js

--- others/daily.js
+++ others/daily.js
@@ -1,6 +1,8 @@
 const { Client, Collection, MessageEmbed } = require(`discord.js`);
-const db = require("quick.db");
-const ms = require("parse-ms");
+const { 
+  PREFIX, 
+} = require(`../config.json`);
+
 
 module.exports = {
     config: {
