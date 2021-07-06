146abca82cf0d36dfdce2f04f187468fa0eefc79
SHVANx69 <78313191+SHVANx69@users.noreply.github.com>
2021-07-06 12:27:08 +0300

    Update daily.js

--- others/daily.js
+++ others/daily.js
@@ -1,4 +1,4 @@
-``` const { MessageEmbed } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const db = require("quick.db");
 const ms = require("parse-ms");
 
@@ -37,4 +37,4 @@ module.exports = {
 
         }
     }
-} ```
+}
