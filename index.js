9b1c967532e9ee6719c8fbcc1761b0ffae2f6cb9
SHVANx69 <78313191+SHVANx69@users.noreply.github.com>
2021-07-10 16:39:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -136,6 +136,8 @@ client.on("message", message => {
       "https://media.discordapp.net/attachments/786897045952790550/802722301984243712/a_66f26e072e89a58c1879c6fa27744bd7.gif",
       "https://media.discordapp.net/attachments/786897045952790550/801054305569865778/uzgun-4.gif",
       "https://media.discordapp.net/attachments/786897044483604490/806288916160315422/image0.gif"
+      "https://media.discordapp.net/attachments/786897045952790550/798719676795715614/Zeyrox_43.gif",
+     
     ];
 
     message.channel
